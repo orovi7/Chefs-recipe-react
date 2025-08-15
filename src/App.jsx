db44b4c28bc5fce7ca632e14c1fcdf46960c70a7
orovi7 <rubaiyatahamimorovi@gmail.com>
2025-08-15 22:03:52 +0600

    navbar is almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      
-      <h1>Vite + React</h1>
-      <p>New here</p>
+     <nav className='flex justify-between p-7 items-center'>
+      {/* name part */}
+      <div>
+       <h1 className='text-3xl font-bold'>Recipe Calories</h1>
+      </div>
+{/* option part */}
+       <div className='flex gap-5'>
+      <ul><li><a>Home</a></li></ul>
+      <ul><li><a>Recipes</a></li></ul>
+      <ul><li><a>About</a></li></ul>
+      <ul><li><a>Search</a></li></ul>
+      </div>
+{/* search bar & user part */}
+       <div>
+        
+        <input type="text" placeholder='Search' />
+      </div>
+     </nav>
       
     </>
   )
