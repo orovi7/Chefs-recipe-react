2df540a94145574d39b06313aa1d769267195b9c
orovi7 <rubaiyatahamimorovi@gmail.com>
2025-08-18 17:27:34 +0600

    added banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-
+// import bannerImage from './chef1.png'
 
 
 const Banner = () => {
     return (
-        <div>
-           
+        // <div style={{backgroundImage : `url(${bannerImage})`}}>
+        <div  className=' h-[500px] md:h-[600px] mb-10    bg-cover bg-center bg-no-repeat ' style={{backgroundImage : "url('/chef1.png')"}}>
+             
+           <div className='text-center  items-center pt-36 flex-row justify-center'>
+        
+             <h1 className='text-5xl font-bold text-white leading-tight'>Discover an exceptional cooking <br />  class tailored for you!</h1>
+          <p className='text-white pt-4 pb-5'>Learn and Master Basic Programming, Data Structures, Algorithm, OOP,
+             Database and solve 500+ coding <br /> problems to become an exceptionally well world-class Programmer.</p>
+         
+           </div>
+           <div className='flex justify-center gap-4 '>
+            <button className='bg-[#0BE58A]  text-black font-bold text-base p-4 px-6 rounded-full'>Explore Now</button>
+            <button className='bg-transparent text-white border-2 font-bold text-base p-4 px-6 rounded-full'>Our Feedback</button>
+           </div>
           
-            <h1>Hello</h1>
-            
-            
         </div>
     );
 };
